b76839a9b659573d6f220ad4f6c329212fcd666e
Asad Babayev <esedbabayev12@gmail.com>
2024-08-26 13:13:55 +0400

    create auth controller

--- Chat-App/controllers/auth.controller.js
+++ Chat-App/controllers/auth.controller.js
@@ -1,3 +1,80 @@
-export const getUsers = async(req,res) => {
+import { User } from "../models/user.model.js";
+import { generateTokenAndSetCookies } from "../generateTokenAndSetCookies.js";
+
+import bcrypt from "bcrypt";
+
+export const signIn = async (request, response) => {
+  const { userName, password } = request.body;
+
+  if (!userName || !password) {
+    response.status(400).send({ message: "please fill all the fields" });
+    return;
+  }
+
+  try {
+    const existingUser = await User.findOne({ userName });
+
+    if (!existingUser) {
+      response.status(400).send({ message: "wrong username or password" });
+      return;
+    }
+
+    const isCorrectPassword = await bcrypt.compare(password, User.password);
+    console.log(isCorrectPassword);
     
-}
+
+    if (!isCorrectPassword) {
+      response.status(400).send({ message: "wrong username or password" });
+      return;
+    }
+  } catch {}
+};
+
+export const signUp = async (request, response) => {
+  const { fullName, userName, password, confirmPassword } = request.body;
+
+  // Check for empty fields
+  if (!fullName || !userName || !password || !confirmPassword) {
+    return response.status(400).send({ message: "Please fill all the fields" });
+  }
+
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({ userName });
+    if (existingUser) {
+      return response.status(400).send({ message: "User already exists" });
+    }
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      return response.status(400).send({ message: "Passwords do not match" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = await User.create({
+      fullName,
+      userName,
+      password: hashedPassword,
+    });
+
+    // Generate token and set cookies
+    generateTokenAndSetCookies(newUser._id, response);
+
+    // Respond with success
+    return response.status(201).send({
+      message: "User successfully created",
+      data: newUser,
+    });
+  } catch (error) {
+    console.error("Error during sign up:", error);
+    return response.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+export const logOut = async (request, response) => {
+  response.cookie();
+};
