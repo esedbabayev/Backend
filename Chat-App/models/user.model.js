1a92275500aca61c2d20e16992f0712149a57c0e
Asad Babayev <esedbabayev12@gmail.com>
2024-08-26 12:44:52 +0400

    create user schema

---
+++ Chat-App/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model("user", UserSchema);
