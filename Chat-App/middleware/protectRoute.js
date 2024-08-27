b5b4152ccd11674f0703f9afd2609ce3d267bb6a
Asad Babayev <esedbabayev12@gmail.com>
2024-08-27 13:10:46 +0400

    create protect route

---
+++ Chat-App/middleware/protectRoute.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const protectRoute = (request, response, next) => {
+  const token = request.cookies.jwt;
+
+  if (!token) {
+    return response
+      .status(500)
+      .send({ message: " No token provided. Authorization failed." });
+  }
+
+  const decode = jwt.decode(token, process.env.JWT_SECRET);
+
+  const { id } = decode;
+
+  request.id = id;
+
+  next();
+};
