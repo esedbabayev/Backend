420afa7e5cf3c8e8c1bc11e5649d93c034949105
asadbabayev <asadyb@code.edu.az>
2024-08-27 14:28:32 +0400

    delete

--- Chat-App/generateTokenAndSetCookie.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateTokenAndSetCookie = (id, response) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET);
-
-  response.cookie("jwt", token, { expiresIn: "15d" });
-};
