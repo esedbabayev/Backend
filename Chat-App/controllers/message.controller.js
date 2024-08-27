e831f0200bcd80d7d25d440e5fc329c99fbe251f
Asad Babayev <esedbabayev12@gmail.com>
2024-08-27 13:11:30 +0400

    get sender id

--- Chat-App/controllers/message.controller.js
+++ Chat-App/controllers/message.controller.js
@@ -1,3 +1,5 @@
-export const sendMessage = async (request, response) => {};
+export const sendMessage = async (request, response) => {
+  const senderId = request.id;
+};
 
 export const getMessages = async (request, response) => {};
