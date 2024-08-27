9144cde1b6d3a665ccd171dde86c4df6e7d7711a
Asad Babayev <esedbabayev12@gmail.com>
2024-08-27 13:11:08 +0400

    import protect route

--- Chat-App/routes/message.routes.js
+++ Chat-App/routes/message.routes.js
@@ -3,7 +3,11 @@ import express from "express";
 // Controllers
 import { sendMessage, getMessages } from "../controllers/message.controller.js";
 
+import { protectRoute } from "../middleware/protectRoute.js";
+
 const router = express.Router();
+
+router.use(protectRoute);
 
 router.post("/:talkingToId", sendMessage);
 router.get("/:talkingToId", getMessages);
