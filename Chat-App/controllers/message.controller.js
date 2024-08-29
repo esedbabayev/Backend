2568acf57534bb3718a05d552635e364318ff7d9
Asad Babayev <esedbabayev12@gmail.com>
2024-08-29 13:09:50 +0400

    sending messages granted

--- Chat-App/controllers/message.controller.js
+++ Chat-App/controllers/message.controller.js
@@ -1,5 +1,22 @@
+import { Message } from "../models/message.model.js";
+
 export const sendMessage = async (request, response) => {
   const senderId = request.id;
+  const { talkingToId: receiverId } = request.params;
+  const { content } = request.body;
+
+  // check empty fields
+  if (!senderId || !receiverId || !content) {
+    return response.status(400).send({ message: "please fill all the fields" });
+  }
+
+  try {
+    const newMessage = await Message.create({ senderId, receiverId, content });
+
+    response.status(201).send(newMessage);
+  } catch {
+    return response.status(500).send({ message: "something went wrong" });
+  }
 };
 
 export const getMessages = async (request, response) => {};
