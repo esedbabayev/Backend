d668d98fbe53eb117365ffd6b0a34e8aaf843e55
Asad Babayev <esedbabayev12@gmail.com>
2024-08-31 00:41:37 +0400

    complete get all users function

--- Chat-App/controllers/user.controller.js
+++ Chat-App/controllers/user.controller.js
@@ -1,3 +1,13 @@
+import { User } from "../models/user.model.js";
+
 export const getAllUsers = async (request, response) => {
-    // Send back all the users except the current user
-}
+  // Send back all the users except the current user
+  const id = request.id;
+
+  try {
+    const allUsers = await User.find({ _id: { $ne: id } });
+    response.status(200).send({ message: "users", data: allUsers });
+  } catch {
+    return response.status(500).send({ message: "something went wrong" });
+  }
+};
