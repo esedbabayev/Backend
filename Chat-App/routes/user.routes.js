5df776d1a13e3cc97ff8da37971854580d14f549
Asad Babayev <esedbabayev12@gmail.com>
2024-08-31 00:40:58 +0400

    add protect route

--- Chat-App/routes/user.routes.js
+++ Chat-App/routes/user.routes.js
@@ -3,8 +3,10 @@ import express from "express";
 // Controllers
 import { getAllUsers } from "../controllers/user.controller.js";
 
+import { protectRoute } from "../middleware/protectRoute.js";
+
 const router = express.Router();
 
-router.get("/", getAllUsers);
+router.get("/", protectRoute, getAllUsers);
 
 export default router;
